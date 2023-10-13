18ffe25ce790cd3f0e6a6ae4b729a86e03528191
megarvit <garvitgarvitjain0306@gmail.com>
2023-10-13 10:30:42 +0530

    task-1

---
+++ task1/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import { Paragraph } from './components/Paragraph';
+
+function App() {
+  return (
+    <div>
+      <Paragraph />
+    </div>
+  );
+}
+
+export default App;
